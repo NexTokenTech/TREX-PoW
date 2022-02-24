79bfd062eddf6e8535759da65016269056bfa32a
kaisuki <1103720832@qq.com>
2022-02-24 15:48:35 +0800

    custom inherent data provider

--- node/src/service.rs
+++ node/src/service.rs
@@ -14,48 +14,6 @@ use sp_core::{Decode, Encode, U256};
 use sp_inherents::{InherentData, InherentIdentifier};
 use std::{sync::Arc, thread, time::Duration};
 
-// This needs to be unique for the runtime.
-const INHERENT_IDENTIFIER: InherentIdentifier = *b"testinh0";
-
-/// Some custom inherent data provider
-struct InherentDataProvider;
-
-#[async_trait::async_trait]
-impl sp_inherents::InherentDataProvider for InherentDataProvider {
-	fn provide_inherent_data(
-		&self,
-		inherent_data: &mut InherentData,
-	) -> Result<(), sp_inherents::Error> {
-		// We can insert any data that implements [`codec::Encode`].
-		inherent_data.put_data(INHERENT_IDENTIFIER, &"hello")
-	}
-
-	/// When validating the inherents, the runtime implementation can throw errors. We support
-	/// two error modes, fatal and non-fatal errors. A fatal error means that the block is invalid
-	/// and this function here should return `Err(_)` to not import the block. Non-fatal errors
-	/// are allowed to be handled here in this function and the function should return `Ok(())`
-	/// if it could be handled. A non-fatal error is for example that a block is in the future
-	/// from the point of view of the local node. In such a case the block import for example
-	/// should be delayed until the block is valid.
-	///
-	/// If this functions returns `None`, it means that it is not responsible for this error or
-	/// that the error could not be interpreted.
-	async fn try_handle_error(
-		&self,
-		identifier: &InherentIdentifier,
-		mut error: &[u8],
-	) -> Option<Result<(), sp_inherents::Error>> {
-		// Check if this error belongs to us.
-		if *identifier != INHERENT_IDENTIFIER {
-			return None
-		}
-
-		// For demonstration purposes we are using a `String` as error type. In real
-		// implementations it is advised to not use `String`.
-		Some(Err(sp_inherents::Error::Application(Box::from(String::decode(&mut error).ok()?))))
-	}
-}
-
 // Our native executor instance.
 pub struct ExecutorDispatch;
 
@@ -156,9 +114,12 @@ pub fn new_partial(
 		sha3pow::MinimalSha3Algorithm,
 		0, // check inherent starting at block 0
 		select_chain.clone(),
-		move |_, ()| async move {
+		|_parent, ()| async {
 			let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
-			Ok(timestamp)
+
+			let data_1 = cp_consensus_inherent::InherentDataProvider::from_default_value();
+
+			Ok((timestamp,data_1))
 		},
 		can_author_with,
 	);
@@ -246,9 +207,12 @@ pub fn new_full(config: Configuration, mining: bool) -> Result<TaskManager, Serv
 				Arc::clone(&network),
 				None,
 				// For block production we want to provide our inherent data provider
-				move |_, ()| async move {
+				|_parent, ()| async {
 					let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
-					Ok(timestamp)
+
+					let data_1 = cp_consensus_inherent::InherentDataProvider::from_default_value();
+
+					Ok((timestamp,data_1))
 				},
 				// time to wait for a new block before starting to mine a new one
 				Duration::from_secs(30),
@@ -290,7 +254,7 @@ pub fn new_full(config: Configuration, mining: bool) -> Result<TaskManager, Serv
 						}
 					}
 				} else {
-					thread::sleep(Duration::new(1, 0));
+					thread::sleep(Duration::new(6, 0));
 				}
 			});
 		}
---
+++ primitives/consensus/cp_provider/src/lib.rs
@@ -0,0 +1,61 @@
+// Copyright (C) 2019-2021 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Inherents for BABE
+
+use sp_inherents::{Error, InherentData, InherentIdentifier};
+
+use sp_std::result::Result;
+
+/// The BABE inherent identifier.
+pub const INHERENT_IDENTIFIER: InherentIdentifier = *b"testinh1";
+
+/// The type of the BABE inherent.
+pub type InherentType = String;
+
+/// Provides the slot duration inherent data for Capsule.
+// TODO: Remove in the future. https://github.com/paritytech/substrate/issues/8029
+#[cfg(feature = "std")]
+pub struct InherentDataProvider {
+    custom_value: InherentType,
+}
+
+
+#[cfg(feature = "std")]
+impl InherentDataProvider {
+    /// Create `Self` while using the system time to get the timestamp.
+    pub fn from_default_value() -> Self {
+        Self {
+            custom_value: "hello".to_string(),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+#[async_trait::async_trait]
+impl sp_inherents::InherentDataProvider for InherentDataProvider {
+    fn provide_inherent_data(&self, inherent_data: &mut InherentData) -> Result<(), Error> {
+        inherent_data.put_data(INHERENT_IDENTIFIER, &self.custom_value)
+    }
+
+    async fn try_handle_error(
+        &self,
+        _: &InherentIdentifier,
+        _: &[u8],
+    ) -> Option<Result<(), Error>> {
+        // There is no error anymore
+        None
+    }
+}
