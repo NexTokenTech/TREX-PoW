a6f17d1d4e4dd285d19d0e2b7ce90c8c2b1906df
kaisuki <1103720832@qq.com>
2022-04-05 18:11:44 +0800

    #CAP-9 Judge release_ block_ Is number greater than the current block height

--- pallets/capsule/src/lib.rs
+++ pallets/capsule/src/lib.rs
@@ -22,6 +22,11 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 	use sp_std::vec::Vec;
+	use scale_info::prelude::string::String;
+	use codec::alloc::string::ToString;
+	// We use `alt_serde`, and Xanewok-modified `serde_json` so that we can compile the program
+	// with serde(features `std`) and alt_serde(features `no_std`).
+	use serde_json::Value;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -87,26 +92,64 @@ pub mod pallet {
 			origin: OriginFor<T>,
 			_from: T::AccountId,
 			cipher_list: Vec<u8>,
-		) -> DispatchResult {
+		) -> DispatchResultWithPostInfo
+		{
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://docs.substrate.io/v3/runtime/origins
 			let who = ensure_signed(origin)?;
 
-			//construct InfoData Struct for CapsuleStorage
-			let owner = who.clone();
-			let ciphers = cipher_list.clone();
-			let capsule_data = CapsuleData::<T> { cipher_list: ciphers, from: owner };
+			let current_block_number = <frame_system::Pallet<T>>::block_number();
 
-			//encode InfoData instance to vec<u8>
-			let capsule_byte_data = capsule_data.encode();
-			// Update storage.
-			<CapsuleStorage<T>>::put(&capsule_byte_data);
+			// get json string from matching chiper_text
+			let cipher_json_str_result = String::from_utf8(cipher_list.clone());
+			let cipher_json_str = match cipher_json_str_result {
+				Ok(ciphers_json) => ciphers_json,
+				Err(_) => "".to_string()
+			};
+			let serde_json_value:Option<Value> = serde_json::from_str(&cipher_json_str).unwrap_or(alt_serde::__private::Some(Value::Null));
+			let cipher_array = match serde_json_value {
+				Some(Value::Array(vector))=>{
+					vector
+				},
+				Some(Value::Null)=>Vec::new(),
+				_=>Vec::new()
+			};
+			let mut is_validate = true;
+			for cipher in cipher_array{
+				let release_height = match &cipher["release_block_num"] {
+					Value::Number(value) => value.as_u64().unwrap(),
+					_ => 0u64
+				} as u32;
+				let current_block:Result<u32,<<T as frame_system::Config>::BlockNumber as TryInto<u32>>::Error> = current_block_number.try_into();
+				let current_block_index = match current_block {
+					Ok(block_num) => block_num,
+					Err(_) => 0u32
+				};
+				if release_height <= current_block_index {
+					is_validate = false;
+					break
+				}
+			}
+			if is_validate == false {
+				Ok((Some(0), Pays::No).into())
+			}else{
+				//construct InfoData Struct for CapsuleStorage
+				let owner = who.clone();
+				let ciphers = cipher_list.clone();
+				let capsule_data = CapsuleData::<T> { cipher_list: ciphers, from: owner };
 
-			// Emit an event.
-			Self::deposit_event(Event::CapsuleDataSent(who, capsule_byte_data));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
+				//encode InfoData instance to vec<u8>
+				let capsule_byte_data = capsule_data.encode();
+				// Update storage.
+				<CapsuleStorage<T>>::put(&capsule_byte_data);
+
+				// Emit an event.
+				Self::deposit_event(Event::CapsuleDataSent(who, capsule_byte_data));
+				// Return a successful DispatchResultWithPostInfo
+				Ok((Some(T::CapsuleWeight::send_capsule_data(cipher_list.len() as u32)), Pays::Yes).into())
+			}
+			// ensure!(is_validate == true, "fail");
 		}
 	}
 }
