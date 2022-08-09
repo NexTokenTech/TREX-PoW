9379dcaa6ac7dfc37b5cb44198a8237180fdf403
kaisuki <1103720832@qq.com>
2022-08-09 11:06:37 +0800

    #TRE-55 nodekey generate use cli

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -14,6 +14,10 @@ pub struct Cli {
 
 #[derive(Debug, clap::Subcommand)]
 pub enum Subcommand {
+	/// Key management cli utilities
+	#[clap(subcommand)]
+	Key(sc_cli::KeySubcommand),
+
 	/// Build a chain specification.
 	BuildSpec(sc_cli::BuildSpecCmd),
 
