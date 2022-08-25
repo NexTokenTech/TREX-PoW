f6473c482e3dcbcb059bfb2adade81879105d247
kaisuki <1103720832@qq.com>
2022-08-25 11:16:50 +0800

    #TRE-62 Organize the difficulty adjustment code and update the config file

--- pallets/difficulty/src/tests.rs
+++ pallets/difficulty/src/tests.rs
@@ -2,6 +2,7 @@ use crate::clamp;
 
 #[test]
 fn test_clamp_algo(){
-	let result = clamp(3600,3000);
+	let result = clamp(3600,3100);
+	println!("result for clamp {}",&result);
 	assert_eq!(1,result,"Difficulty does not need to increase by 1");
 }
