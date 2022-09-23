be7c088750362a05c6e87716462030b2753c9002
kaisuki <1103720832@qq.com>
2022-09-23 11:14:45 +0800

    1.Replce jsonrpc with jsonrpsee for Pallet-Storage. 2.Add Rpc for Pallet-Difficulty 3.Expose Pallet-Difficulty Rpc method as get_avg_blocktime.

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -36,17 +36,23 @@ pub fn create_full<C, P>(
 		C: Send + Sync + 'static,
 		C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>,
 		C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+		C::Api: pallet_storage_rpc::SumStorageRuntimeApi<Block>,
+		C::Api: pallet_difficulty_rpc::DiffAdjustmentRuntimeApi<Block>,
 		C::Api: BlockBuilder<Block>,
 		P: TransactionPool + 'static,
 {
 	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
 	use substrate_frame_rpc_system::{System, SystemApiServer};
+	use pallet_storage_rpc::{SumStorage, SumStorageApiServer};
+	use pallet_difficulty_rpc::{DiffAdjustment, DiffAdjustmentApiServer};
 
 	let mut module = RpcModule::new(());
 	let FullDeps { client, pool, deny_unsafe } = deps;
 
 	module.merge(System::new(client.clone(), pool.clone(), deny_unsafe).into_rpc())?;
-	module.merge(TransactionPayment::new(client).into_rpc())?;
+	module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+	module.merge(DiffAdjustment::new(client.clone()).into_rpc())?;
+	module.merge(SumStorage::new(client).into_rpc())?;
 
 	// Extend this RPC with a custom API by using the following syntax.
 	// `YourRpcStruct` should have a reference to a client, which is needed
